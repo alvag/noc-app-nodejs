ec14316a1b17f8570c8bc40ca0f499526a2b5b4a
Max Alva <alva85@gmail.com>
2023-10-13 08:19:33 -0500

    FileSystem saveLog

--- src/domain/infrastructure/datasources/file-system.datasource.ts
+++ src/domain/infrastructure/datasources/file-system.datasource.ts
@@ -4,7 +4,7 @@ import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
 
 export class FileSystemDatasource implements LogDatasource {
     private readonly logPath = 'logs';
-    private readonly allLogsPath = `${ this.logPath }/logs-low.log`;
+    private readonly allLogsPath = `${ this.logPath }/logs-all.log`;
     private readonly mediumLogsPath = `${ this.logPath }/logs-medium.log`;
     private readonly highLogsPath = `${ this.logPath }/logs-high.log`;
 
@@ -29,7 +29,17 @@ export class FileSystemDatasource implements LogDatasource {
         return Promise.resolve( [] );
     }
 
-    saveLog( log: LogEntity ): Promise<void> {
-        return Promise.resolve( undefined );
+    async saveLog( log: LogEntity ): Promise<void> {
+        const logAsJson = `${ JSON.stringify( log ) }\n`;
+        fs.appendFileSync( this.allLogsPath, logAsJson );
+
+        if ( log.level === LogSeverityLevel.low ) return;
+
+        if ( log.level === LogSeverityLevel.medium ) {
+            fs.appendFileSync( this.mediumLogsPath, logAsJson );
+        } else {
+            fs.appendFileSync( this.highLogsPath, logAsJson );
+        }
+
     }
 }
